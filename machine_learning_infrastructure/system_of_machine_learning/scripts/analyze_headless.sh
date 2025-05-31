4cc047ee66403442da206a10d8d519dc9ef9b339
bdg_xps_desktop <blake.degarza@gmail.com>
2025-05-31 09:16:56 -0500

    removed useless info

--- machine_learning_infrastructure/system_of_machine_learning/scripts/analyze_headless.sh
+++ machine_learning_infrastructure/system_of_machine_learning/scripts/analyze_headless.sh
@@ -1,9 +1,9 @@
-GHIDRA_HOME=/home/bdg/Downloads/ghidra_11.2.1_PUBLIC_20241105/ghidra_11.2.1_PUBLIC/
+GHIDRA_HOME=/path/to/ghidra_11.2.1_PUBLIC_20241105/ghidra_11.2.1_PUBLIC/
 
 $GHIDRA_HOME/support/analyzeHeadless \
-  /home/bdg/ \
-  router \
-  -process "CISCO/cpio-root/asa/bin/lina" \
-  -scriptPath /home/bdg/ghidra_scripts \
+  /path/to/ \
+  <your_project> \
+  -process "<your_binary>" \
+  -scriptPath /path/to/ghidra_scripts \
   -postScript ExtractFeatures
 
